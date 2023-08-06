4d1f6036d83ecbeaf172fba85fd88882e7d46130
Guido Schmidt <git@guidoschmidt.cc>
2023-08-06 14:25:15 +0200

    EDIT/2d-marching-squares: moved marching squares algorithm to lygia

--- 2d-marching-squares/src/glsl/frag.glsl
+++ 2d-marching-squares/src/glsl/frag.glsl
@@ -4,23 +4,21 @@ uniform vec2 u_mouse;
 uniform float u_time;
 uniform vec2 u_resolution;
 
+in vec2 v_uv;
+
 out vec4 fragColor;
 
-#include "../../../lygia/generative/voronoi.glsl"
-#include "../../../lygia/generative/curl.glsl"
-#include "../../../lygia/generative/worley.glsl"
-#include "../../../lygia/math/highPass.glsl"
-#include "../../../lygia/color/palette.glsl"
-
-#include "./marching_squares.glsl"
+#include "../../../lygia/space/ratio.glsl"
+#include "../../../lygia/generative/pnoise.glsl"
+#include "../../../lygia/sample/marchingSquares.glsl";
 
 void main() {
   vec4 final = vec4(1.0, 0.0, 0.0, 1.0);
 
-  vec2 uv = gl_FragCoord.xy / u_resolution * vec2(u_resolution.x / u_resolution.y, 1.0);
-  float t = u_time * 0.25;
+  vec2 st = ratio(v_uv, u_resolution);
 
-  vec3 ms = marchin_squares(uv + t, u_mouse.x * 60.0, u_mouse.y, u_resolution);
-  final.rgb = vec3(ms.r);
+  vec2 ms = marchinSquares(st, u_mouse.x * 60.0, u_mouse.y, u_resolution);
+  final.rgb = vec3(ms.r, ms.g, 0.0);
+
   fragColor = final;
 }
--- 2d-marching-squares/src/glsl/vert.glsl
+++ 2d-marching-squares/src/glsl/vert.glsl
@@ -2,7 +2,11 @@ uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
 in vec3 position;
+in vec2 uv;
+
+out vec2 v_uv;
 
 void main() {
+  v_uv = uv;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
